0ad99cacae75c51e4503128d5ab2b1bfa304d387
listrz- <poplkun2915@gmail.com>
2025-04-24 10:32:27 +0900

    Add File

---
+++ DataBase/kensaku.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎</title>
+</head>
+<body>
+    <?php
+    $code = $_POST['code'];
+    $dsn='mysql:dbname=phpkiso;host=localhost;charset=utf8';
+    $user='root';
+    $password= '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql='SELECT * FROM `anketo` WHERE code=?';
+    $stmt= $dbh->prepare($sql);
+    $data[]=$code;
+    $stmt->execute($data);
+
+    while (1) {
+        $rec= $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($rec==false) {
+            break;
+        }
+        print $rec['code'];
+        print $rec['nickname'];
+        print $rec['email'];
+        print $rec['goiken'];
+        print '<br>';
+    }
+
+    $dbh= null;
+    ?>
+</body>
+</html>
