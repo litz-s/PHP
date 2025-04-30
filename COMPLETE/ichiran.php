e6b7bbb102cf7e269770c2ae33b33643526b216b
listrz- <poplkun2915@gmail.com>
2025-04-30 09:59:31 +0900

    Add Files
    完成版ー

---
+++ COMPLETE/ichiran.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎</title>
+</head>
+<body>
+    <?php
+    $dsn='mysql:dbname=phpkiso;host=localhost;charset=utf8';
+    $user='root';
+    $password= '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql='SELECT * FROM `anketo` WHERE 1';
+    $stmt= $dbh->prepare($sql);
+    $stmt->execute();
+
+    while (1) {
+        $rec= $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($rec==false) {
+            break;
+        }
+        print $rec['code'];
+        print $rec['nickname'];
+        print $rec['email'];
+        print $rec['goiken'];
+        print '<br>';
+    }
+
+    $dbh= null;
+    ?>
+
+    <br>
+    <a href="menu.html">メニューに戻る</a>
+</body>
+</html>
