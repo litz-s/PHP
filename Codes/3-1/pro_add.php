25063a71ad55123acef27bdd240255c1725fed6c
listrz- <poplkun2915@gmail.com>
2025-06-05 14:23:48 +0900

    Add Files

---
+++ Codes/3-1/pro_add.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <title>staff</title>
+</head>
+<body>
+    商品追加<br>
+    <br />
+    <form method="post" action="pro_add_check.php">
+        商品名を入力してください。<br>
+        <input type="text" name="name" style="width:200px"><br />
+        価格を入力してください。<br>
+        <input type="text" name="price" style="width:50px"><br />
+        <input type="button" onclick="history.back()" value="戻る" >
+        <input type="submit" value="OK">
+    </form>
+</body>
+</html>
