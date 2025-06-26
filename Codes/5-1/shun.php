3438e8016bb873f99e410a2b8458f10593f923b3
listrz- <poplkun2915@gmail.com>
2025-06-26 11:47:48 +0900

    Add Files

---
+++ Codes/5-1/shun.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <title>asobi</title>
+</head>
+<body>
+    <?php
+
+    $tsuki = $_POST['tsuki'];
+
+    $yasai[] = '';
+    $yasai[] = 'ブロッコリー';
+    $yasai[] = 'カリフラワー';
+    $yasai[] = 'レタス';
+    $yasai[] = 'みつば';
+    $yasai[] = 'アスパラガス';
+    $yasai[] = 'セロリ';
+    $yasai[] = 'ナス';
+    $yasai[] = 'ピーマン';
+    $yasai[] = 'オクラ';
+    $yasai[] = 'さつまいも';
+    $yasai[] = '大根';
+    $yasai[] = 'ほうれんそう';
+
+    print $tsuki;
+    print '月は';
+    print $yasai[$tsuki];
+    print 'が旬です。';
+
+    ?>
+</body>
+</html>
