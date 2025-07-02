e3d0bf8105d31f0ca833b8cf1b7ebdf9bf57ccb5
listrz- <poplkun2915@gmail.com>
2025-07-02 13:47:40 +0900

    Add Files

---
+++ Codes/6-4/clear_cart.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+$_SESSION = array();
+if (isset($_COOKIE[session_name()]) == true) {
+    setcookie(session_name(), '', time() - 42000, '/');
+}
+session_destroy();
+?>
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <title>login</title>
+</head>
+<body>
+    カートを空にしました。<br />
+    <br />
+    <a href = "../staff_login/staff_login.html">ログイン画面へ</a>
+</body>
+</html>
